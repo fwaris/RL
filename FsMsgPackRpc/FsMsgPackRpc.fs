ee0e50312d70182c2ac02f9535d9abff1f18dce9
Faisal Waris <FaisalWaris@live.com>
2022-08-03 09:33:11 -0400

    Add project files.

---
+++ FsMsgPackRpc/FsMsgPackRpc.fs
@@ -0,0 +1,105 @@
+﻿namespace FsMsgPackRpc
+open System
+open System.IO
+open System.Buffers
+open MessagePack
+open MessagePack.Resolvers
+open MessagePack.FSharp
+open System.Net
+open System.Net.Sockets
+open System.Threading
+open System.Collections.Concurrent
+
+type Msg = Req of (int*(Type*AsyncReplyChannel<obj>)) | Resp of int*obj*byte[]
+
+module Matcher = 
+    //agent to match responses to pending requests
+    //Note: MsgPack rpc allows for out-of-order responses 
+    let createAgent options (cts:CancellationTokenSource) =
+            (fun (inbox:MailboxProcessor<Msg>) ->
+                let pending = new System.Collections.Generic.Dictionary<int,(Type*AsyncReplyChannel<obj>)>()
+                async {
+                    while not cts.IsCancellationRequested do
+                        try                        
+                            match! inbox.Receive() with
+                            | Req (id,rc) -> pending.Add(id,rc)                        
+                            | Resp (id,err,resp) ->                                
+                                match pending.TryGetValue id with
+                                | true,(t,rc) -> 
+                                    pending.Remove(id) |> ignore
+                                    use ms = new MemoryStream(resp)
+                                    let! obj = task {return! MessagePackSerializer.DeserializeAsync(t,ms,options=options)} |> Async.AwaitTask
+                                    let isnull = (obj = null)
+                                    rc.Reply(obj)
+                                | _ -> printfn $"unmatched response for id {id}"
+                        with ex -> 
+                            printfn "%A" ex.Message
+            })
+
+type Client(options:MessagePack.MessagePackSerializerOptions) =    
+    let cts = new CancellationTokenSource()
+    let tcpClient = new TcpClient()    
+    let mutable id = -1
+    let nextId() = Interlocked.Increment(&id)
+    let mbp = lazy(MailboxProcessor.Start(Matcher.createAgent options cts,cts.Token))
+    let sem = new ManualResetEvent(true)
+
+    //message receive loop
+    let receive() =
+        task {
+            try
+                use strm = new MessagePackStreamReader(tcpClient.GetStream(),leaveOpen=true)
+                while not cts.IsCancellationRequested do
+                    let! h = strm.ReadArrayHeaderAsync(cts.Token)
+                    let! m = strm.ReadAsync(cts.Token)
+                    let! id = strm.ReadAsync(cts.Token)
+                    let! err = strm.ReadAsync(cts.Token)
+                    let! data = strm.ReadAsync(cts.Token)
+                    if h > 0 && m.HasValue && id.HasValue && err.HasValue && data.HasValue then                        
+                        let mutable idV = id.Value
+                        let msgId = MessagePackSerializer.Deserialize<int>(&idV)                        
+                        let mutable errV = err.Value
+                        let errObj = MessagePackSerializer.Deserialize<obj>(&errV)
+                        let bytes = data.Value.ToArray()
+                        let resp = Resp(msgId,errObj,bytes)
+                        printfn "%A" resp
+                        mbp.Value.Post resp
+            with ex -> 
+                printfn "%A" ex.Message
+        }        
+
+    member _.Connect (address:string,port:int) =  
+        tcpClient.Connect(address,port)        
+        receive() |> ignore
+
+    member private _.Disconnect() =
+        cts.CancelAfter(100)
+        tcpClient.Dispose()
+        sem.Dispose()
+
+    member _.TcpClient = tcpClient
+    
+    ///sends a message and waits for response (threadsafe)
+    member _.Call<'req,'resp> (method:string,req:'req) =
+        let msgId = nextId()
+        let reqMsg:obj[] = [|0; msgId; method; req |]        
+        task {        
+            let! resp = mbp.Value.PostAndTryAsyncReply((fun rc -> Req (msgId, (typeof<'resp>, rc))),timeout=5000) |> Async.StartChild
+            let! _ = Async.AwaitWaitHandle sem
+            let _ = sem.Reset()
+            try
+                do! MessagePackSerializer.SerializeAsync(tcpClient.GetStream(),reqMsg)
+            finally
+                sem.Set() |> ignore
+            printfn "written"
+            match! resp with 
+            | Some o -> return (o :?> 'resp)
+            | None -> return failwith "timeout"
+        }
+        
+    interface IDisposable with
+        member this.Dispose() = this.Disconnect()
+                
+
+
+        
---
+++ FsMsgPackRpc/scripts/sandbox.fsx
@@ -0,0 +1,32 @@
+﻿#r "nuget: MessagePack.FSharpExtensions"
+#load "../FsMsgPackRpc.fs"
+open System
+open MessagePack
+open MessagePack.FSharp
+open MessagePack.Resolvers
+open FsMsgPackRpc
+
+let address = "localhost"
+let port = 41451
+let resolver = Resolvers.CompositeResolver.Create(
+                FSharpResolver.Instance,
+                StandardResolver.Instance)
+let options = MessagePackSerializerOptions.Standard.WithResolver(resolver)
+
+
+type AimC(options) =
+    inherit Client(options) 
+
+    member this.getServerVersion() = 
+        let name = nameof this.getServerVersion
+        base.Call<_,int>(name,[||]) 
+        
+let c1 = new AimC(options)
+
+c1.Connect(address,port)
+c1.getServerVersion() |> Async.AwaitTask |> Async.RunSynchronously
+
+(c1 :> IDisposable).Dispose()
+
+
+        
