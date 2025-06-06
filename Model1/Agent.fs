c1f84abee135ef5de854a285e769b6aca8e5339f
Faisal Waris <Faisal.Waris1@t-mobile.com>
2025-06-06 08:44:19 -0400

    improve logging

--- Model1/Agent.fs
+++ Model1/Agent.fs
@@ -87,7 +87,7 @@ let computeRewards parms env s action =
                 interReward                
         if verbosity.isHigh || verbosity.IsMed && isDone then
             let act = if isDone then s.AgentBook |> List.map _.Action |> List.countBy id |> List.sortBy fst |> sprintf "%A" else $"{action}"
-            printfn $"{s.AgentId}-{s.TimeStep}|{s.Step.Num} - P:%0.3f{avgP}, OnHand:{s.CashOnHand}, S:{s.Stock}, R:{reward}, A:{act}, Exp:{s.Step.ExplorationRate} Gain:{sGain}"
+            printfn $"{s.AgentId}-{s.TimeStep}|{s.Step.Num} - P:%0.3f{avgP}, OnHand:{s.CashOnHand}, S:{s.Stock}, R:{reward}, A:{act}, Exp:%0.2f{s.Step.ExplorationRate} Gain:{sGain}"
         let logLine = $"{s.AgentId},{s.Epoch},{s.TimeStep},{action},{avgP},{s.CashOnHand},{s.Stock},{reward},{sGain},{parms.RunId},{env.StartIndex},{isDone}"
         if parms.LogSteps then
             Data.logger.Post (s.Epoch,parms.RunId,logLine)
